aaa2aed91a89de721d067c9105035cd1753aa8fa
GMainardi <guidoluis.gm@gmail.com>
2024-12-05 23:41:47 -0300

    day 25 ⭐⭐

--- day25/a.py
+++ day25/a.py
@@ -1,89 +1,82 @@
+from collections import deque
 import random
+import sys
 
-input = {}
-
-with open('day25/example.txt') as f:
-    lines = f.readlines()
-
-    for line in lines:
-
-        key = line.strip().split(':')[0]
-        values = line.strip().split(':')[1].split(' ')[1:]
-
-        input[key] = input.get(key, []) + values
-        for value in values:
-            input[value] = input.get(value, []) + [key]
-
-def remove_connections(input, c1, c2, c3):
-    input[c1[0]].remove(c1[1])
-    input[c1[1]].remove(c1[0])
-    input[c2[0]].remove(c2[1])
-    input[c2[1]].remove(c2[0])
-    input[c3[0]].remove(c3[1])
-    input[c3[1]].remove(c3[0])
-
-def count_connections(input, start):
-    queue = [start]
-    visited = set(queue.copy())
-
-    while queue:
-        current = queue.pop(0)
-        
-        if current not in input.keys():
-            continue
-
-        for connection in input[current]:
-            if connection not in visited:
-                visited.add(connection)
-                queue.append(connection)
-
-    return visited
-
-def find_path(input, curr, end, visited):
-    visited.add(curr)
-
-    if curr == end:
-        return [curr]
-
-    for connection in input[curr]:
-        if connection not in visited:
-            path = find_path(input, connection, end, visited)
-            if path:
-                return [curr] + path
-
-    return []
-
-
-total_count = count_connections(input, list(input.keys())[0])
+sys.setrecursionlimit(100_000)
 
 connections = {}
-for key in input.keys():
-    for value in input[key]:
-        if (value, key) not in connections.keys():
-            connections[(key, value)] = 0
+with open('input.txt', 'r') as f:
+    input = f.read().splitlines()
+    for line in input:
+        key = line.split(':')[0]
+        values = line.split(':')[1].split(' ')[1:]
+        connections[key] = connections.get(key, []) + values
+        for value in values:
+            connections[value] = connections.get(value, []) + [key]
 
-for i in range(0, 100_000):
+def count_connected(graph, start):
+    stack = [start]
+    visited = set(stack.copy())
+    while stack:
+        node = stack.pop()
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                stack.append(neighbor)
+                visited.add(neighbor)
+    return len(visited)
 
-    start = random.choice(list(input.keys()))
-    end = random.choice(list(input.keys()))
+def find_path_bfs(graph, start, end):
+
+    if start not in graph or end not in graph:
+        return None
+
+    queue = deque([[start]]) 
+    visited = set() 
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+
+        if node == end:
+            return path
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph[node]:
+                new_path = path + [neighbor]
+                queue.append(new_path)
+
+    return None
+
+
+total_nodes = len(connections.keys())
+
+connections_counts = {}
+
+for _ in tqdm(range(1_000)):
+    start = random.choice(list(connections.keys()))
+    end = random.choice(list(connections.keys()))
 
     while start == end:
-        end = random.choice(list(input.keys()))
+        end = random.choice(list(connections.keys()))
 
-    path = find_path(input, start, end, set())
 
-    for c1, c2 in zip(path, path[1:]):
-        if (c1, c2) in connections.keys():
-            connections[(c1, c2)] += 1
+    path = find_path_bfs(connections, start, end)
+
+    for n1, n2 in zip(path, path[1:]):
+        if (n1, n2) in connections_counts:
+            connections_counts[(n1, n2)] += 1
+        elif (n2, n1) in connections_counts:
+            connections_counts[(n2, n1)] += 1
         else:
-            connections[(c2, c1)] += 1
+            connections_counts[(n1, n2)] = 1
 
-c1, c2, c3 = sorted(connections, key=connections.get, reverse=True)[:3]
+top_connections = sorted(connections_counts.items(), key=lambda x: x[1], reverse=True)[:3]
 
-print(c1, c2, c3)
+for (c1, c2), _ in top_connections:
+    connections[c1].remove(c2)
+    connections[c2].remove(c1)
 
-remove_connections(input, c1, c2, c3)
+group_nodes = count_connected(connections, list(connections.keys())[0])
 
-new_count = count_connections(input, list(input.keys())[0])
-
-print((len(total_count) - len(new_count)) * len(new_count))
+print(group_nodes * (total_nodes - group_nodes))
